894ba7fc7b7e518d6c52bdd57252f0a5bc496610
Jesse Frey <jmfrey@alaska.edu>
2013-08-15 15:14:54 -0800

    Changed to symbolic thread priorities

--- main.c
+++ main.c
@@ -87,9 +87,9 @@ int main(void){
   stackInit(stack3,sizeof(stack3));
 
   //create tasks
-  ctl_task_run(&tasks[0],1,ACDS_events,NULL,"cmd_parse",sizeof(stack1)/sizeof(stack1[0])-2,stack1+1,0);
-  ctl_task_run(&tasks[1],2,terminal,(void*)&uart_term,"terminal",sizeof(stack2)/sizeof(stack2[0])-2,stack2+1,0);
-  ctl_task_run(&tasks[2],10,sub_events,NULL,"sub_events",sizeof(stack3)/sizeof(stack3[0])-2,stack3+1,0);
+  ctl_task_run(&tasks[0],BUS_PRI_LOW,ACDS_events,NULL,"cmd_parse",sizeof(stack1)/sizeof(stack1[0])-2,stack1+1,0);
+  ctl_task_run(&tasks[1],BUS_PRI_NORMAL,terminal,(void*)&uart_term,"terminal",sizeof(stack2)/sizeof(stack2[0])-2,stack2+1,0);
+  ctl_task_run(&tasks[2],BUS_PRI_HIGH,sub_events,NULL,"sub_events",sizeof(stack3)/sizeof(stack3[0])-2,stack3+1,0);
   
   mainLoop();
 }
